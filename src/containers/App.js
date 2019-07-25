de9e69788c4d4bbdd3288e91ec4966315267b108
baldm0mma <jev.forsberg@gmail.com>
2019-07-25 10:07:52 -0600

    Characters successfully populating to redux store

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { getAllCharacters } from '../apiCalls/apiCalls';
+import { sendAllCharacters, sendSingleCharacter } from '../actions';
 import './App.css';
 
 export class App extends Component {
@@ -7,6 +9,7 @@ export class App extends Component {
   componentDidMount = async () => {
     const results = await getAllCharacters();
     console.log(results);
+    this.props.onReceivingAllCharacters(results);
   }
 
   render = () => {
@@ -14,4 +17,13 @@ export class App extends Component {
   }
 }
 
-export default App;
+export const mapStatetoProps = ({ allCharacters, currentCharacter }) => ({
+  allCharacters,
+  currentCharacter
+});
+
+export const mapDispatchToProps = dispatch => ({
+  onReceivingAllCharacters: (results) => dispatch(sendAllCharacters(results))
+})
+
+export default connect(mapStatetoProps, mapDispatchToProps)(App);
