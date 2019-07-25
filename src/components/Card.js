f908f946412fdbb2f53c8e7bf31881ed5d750c7a
baldm0mma <jev.forsberg@gmail.com>
2019-07-25 11:32:48 -0600

    Add Card component, show name and photo on screen

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Card = ({ character }) => {
+  console.log(character);
+  return (
+    <article>
+      <h2>{character.name}</h2>
+      <img src={character.image} alt={`mugshot of ${character.name}`} />
+    </article>
+  );
+};
