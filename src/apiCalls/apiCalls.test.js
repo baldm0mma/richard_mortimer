fc43b2995bccdcf82cc89d3f49641aef69b8af47
baldm0mma <jev.forsberg@gmail.com>
2019-07-25 18:24:42 -0600

    Add should be called with the correct url test

--- src/apiCalls/apiCalls.test.js
+++ src/apiCalls/apiCalls.test.js
@@ -1,17 +1,29 @@
 import { getAllCharacters } from './apiCalls';
-import { allCharactersUrl } from './urls';
 
 describe('Api calls', () => {
+  let url = 'https://rickandmortyapi.com/api/character/?page=undefined';
+  let mockAllCharacters = [
+    { id: 1, name: 'Mr. Poopybutthole' },
+    { id: 2, name: 'Noob-Noob' }
+  ];
+  // let mockCharacter = { id: 1, name: 'Mr. Poopybutthole' };
   beforeEach(() => {
-    let mockAllCharacters = [{id: 1, name: 'Mr. Poopybutthole'}, {id: 2, name: 'Noob-Noob'}]
-    let mockCharacter = {id: 1, name: 'Mr. Poopybutthole'};
     window.fetch = jest.fn().mockImplementation(() => {
-      
-    })
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockAllCharacters)
+      });
+    });
   });
   describe('getAllCharacters', () => {
     it('should be called with the correct url', () => {
+      getAllCharacters();
+      expect(window.fetch).toHaveBeenCalledWith(url);
+    });
 
+    it('should return a parsed result', async () => {
+      const result = await getAllCharacters();
+      expect(result).toEqual(mockAllCharacters);
     });
   });
 });
