09c49f242f3280e4942d693b0ba70e4574a79191
baldm0mma <jev.forsberg@gmail.com>
2019-07-26 18:50:01 -0600

    Add react typer dependency that works

--- src/containers/Monitor.js
+++ src/containers/Monitor.js
@@ -8,79 +8,46 @@ export class Monitor extends Component {
     super();
     this.state = {
       counter: 0,
-      name: ''
     };
   }
 
-  componentDidMount = async () => {
-    await console.log('props', this.props);
-    this.setState({ name: this.props.userName });
-    await console.log(this.state.name);
-    console.log(this.props.username);
-  }
-
-
   login = () => {
     return (
-      <Typist>
-        {`Hello ${this.state.name}, your Interdimentional Criminal Code Enforcement Organization login credentials have been accepted... please wait while we log you in...`}
-      </Typist>
-    )
+      <section>
+        <Typist avgTypingDelay={50}>
+          {`Hello ${this.props.userName}, your Interdimentional Criminal Code Enforcement Organization login credentials have been accepted...`}
+          <br />
+          <Typist.Delay ms={1000} />
+          {`Please wait while we log you in...`}
+          <br />
+          <Typist.Delay ms={1000} />
+          {`...`}
+          <br />
+          <Typist.Delay ms={1000} />
+          {`...`}
+          <br />
+          <Typist.Delay ms={1000} />
+          {`...`}
+          <br />
+          <Typist.Delay ms={1000} />
+          {`...`}
+          <br />
+          <br />
+          <Typist.Delay ms={2000} />
+          {`I am G.L.E.A, the AI in charge of the Galictic Law Enforcement Atlas. My database contains information on some of the most vicious and vile criminals in the know multivers; along with thier known associates.`}
+          <br />
+          <Typist.Delay ms={2000} />
+          {`${this.props.userName}, you have summoned here by the Intergalactic Council on Multiverse Security and Defense to assist in the apprehension, incarceration, and service of justice to these noxious villians.`}
+          <br />
+          <Typist.Delay ms={3000} />
+          {`We wish you all the best in your hunt, ${this.props.userName}...`}
+        </Typist>
+      </section>
+    );
   };
 
-  // introToSystem = () => {
-  //   return (
-  //     <p className={this.state.counter === 1 && 'typing'}>
-  //       {
-  //         'I am G.L.E.A, the AI in charge of the Galictic Law Enforcement Atlas. My database contains information on some of the most vicious and vile criminals in the know multivers; along with thier known associates.'
-  //       }
-  //     </p>
-  //   );
-  // };
-
-  // callToAction = () => {
-  //   return (
-  //     <p className='typing'>{`${
-  //       this.props.userName
-  //     }, you have summoned here by the Intergalactic Council on Multiverse Security and Defense to assist in the apprehension, incarceration, and service of justice to these noxious villians.`}</p>
-  //   );
-  // };
-
-  // bestWishes = () => {
-  //   return (
-  //     <p className='typing'>{`We wish you all the best in your hunt, ${
-  //       this.props.userName
-  //     }...`}</p>
-  //   );
-  // };
-
-  // display = () => {
-  //   const introduction = [
-  //     <p className={this.state.typing}>{`Hello ${
-  //       this.props.userName
-  //     }, your Interdimentional Criminal Code Enforcement Organization login credentials have been accepted... please wait while we log you in...`}</p>,
-  //     <p className={this.state.typing}>
-  //       {
-  //         'I am G.L.E.A, the AI in charge of the Galictic Law Enforcement Atlas. My database contains information on some of the most vicious and vile criminals in the know multivers; along with thier known associates.'
-  //       }
-  //     </p>,
-  //     <p className='typing'>{`${
-  //       this.props.userName
-  //     }, you have summoned here by the Intergalactic Council on Multiverse Security and Defense to assist in the apprehension, incarceration, and service of justice to these noxious villians.`}</p>,
-  //     <p className='typing'>{`We wish you all the best in your hunt, ${
-  //       this.props.userName
-  //     };`}</p>
-  //   ];
-  //   return introduction[this.state.counter];
-  // };
-
   render = () => {
-    return (
-      <section>
-        {this.state.userName && this.login()}
-      </section>
-      
-    );
+    return <section>{this.props.userName && this.login()}</section>;
   };
 }
 
@@ -89,4 +56,7 @@ export const mapStateToProps = ({ userName, page }) => ({
   page
 });
 
-export default connect(mapStateToProps, null)(Monitor);
+export default connect(
+  mapStateToProps,
+  null
+)(Monitor);
