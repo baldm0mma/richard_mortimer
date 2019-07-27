197588665e86e964b557f2da172928b8a8000995
baldm0mma <jev.forsberg@gmail.com>
2019-07-27 17:03:25 -0600

    Add styling to associates display

--- src/containers/SingleFocus/SingleFocus.js
+++ src/containers/SingleFocus/SingleFocus.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import Typist from 'react-typist';
 import './SingleFocus.css';
 
 export const SingleFocus = props => {
-
-  const focusedCharacter = props.allCharacters.find(char => char.id === props.currentCharacter.id);
+  const focusedCharacter = props.allCharacters.find(
+    char => char.id === props.currentCharacter.id
+  );
 
   const allAssociates = () => {
     return focusedCharacter.episode.reduce((associates, episode) => {
@@ -14,14 +15,18 @@ export const SingleFocus = props => {
         if (character.episode.includes(episode)) {
           associates.push(character);
         }
-      })
+      });
       return associates;
     }, []);
-  }
+  };
 
   const topAssociates = () => {
-    return [allAssociates()[allAssociates().length - 2], allAssociates()[allAssociates().length - 3], allAssociates()[allAssociates().length - 4]];
-  }
+    return [
+      allAssociates()[allAssociates().length - 2],
+      allAssociates()[allAssociates().length - 3],
+      allAssociates()[allAssociates().length - 4]
+    ];
+  };
 
   return (
     <section className='all_specific_content'>
@@ -29,58 +34,63 @@ export const SingleFocus = props => {
         <p>Back To Database</p>
         <p>Log Out</p>
       </header>
-      <Typist cursor={{ hideWhenDone: true }}>
-        <aside>
-          <h1>Name: {focusedCharacter.name}</h1>
-          <p>Status: {focusedCharacter.status}</p>
-          <p>Species: {focusedCharacter.species}</p>
-          <p>Gender: {focusedCharacter.gender}</p>
-          <p>Origin: {focusedCharacter.origin.name}</p>
-          <p>Current Location: {focusedCharacter.location.name}</p>
-        </aside>
-      </Typist>
-      <div>
-        <section>
-          <article>
-            {/* <img src={} alt={`mugshot of...`}></img> */}
-          </article>
-        </section>
-        <section>
-          <h2>Known Associates</h2>
-          <article>
-            {/* <img src={} alt={`mugshot of...`}></img> */}
-            <Typist cursor={{ hideWhenDone: true }}>
-              <p>Name: {topAssociates()[0].name}</p>
-              <p>Species: {topAssociates()[0].species}</p>
-              <p>Status: {topAssociates()[0].status}</p>
-            </Typist>
-          </article>
-          <article>
-            {/* <img src={} alt={`mugshot of...`}></img> */}
-            <Typist cursor={{ hideWhenDone: true }}>
-              <p>Name: {topAssociates()[1].name}</p>
-              <p>Species: {topAssociates()[1].species}</p>
-              <p>Status: {topAssociates()[1].status}</p>
-            </Typist>
-          </article>
-          <article>
-            {/* <img src={} alt={`mugshot of...`}></img> */}
-            <Typist cursor={{ hideWhenDone: true }}>
-              <p>Name: {topAssociates()[2].name}</p>
-              <p>Species: {topAssociates()[2].species}</p>
-              <p>Status: {topAssociates()[2].status}</p>
-            </Typist>
-          </article>
+      <div className='styling_container'>
+        <Typist className='focus_details' cursor={{ hideWhenDone: true }}>
+          <aside>
+            <img className='associate_img' src={focusedCharacter.image} alt={`mugshot of...`}></img>
+            <h1>Name: {focusedCharacter.name}</h1>
+            <p>Status: {focusedCharacter.status}</p>
+            <p>Species: {focusedCharacter.species}</p>
+            <p>Gender: {focusedCharacter.gender}</p>
+            <p>Origin: {focusedCharacter.origin.name}</p>
+            <p>Current Location: {focusedCharacter.location.name}</p>
+          </aside>
+        </Typist>
+        <section className='associates_container'>
+          <h3 className='associate_title'>Known Associates</h3>
+          <div className='associates_styling'>
+            <article className='associate 1'>
+              <img className='associate_img' src={topAssociates()[0].image} alt={`mugshot of...`}></img>
+              <Typist cursor={{ hideWhenDone: true }}>
+                <p>Name: {topAssociates()[0].name}</p>
+                <p>Species: {topAssociates()[0].species}</p>
+                <p>Status: {topAssociates()[0].status}</p>
+              </Typist>
+            </article>
+            <article className='associate 2'>
+              <img className='associate_img' src={topAssociates()[1].image} alt={`mugshot of...`}></img>
+              <Typist cursor={{ hideWhenDone: true }}>
+                <p>Name: {topAssociates()[1].name}</p>
+                <p>Species: {topAssociates()[1].species}</p>
+                <p>Status: {topAssociates()[1].status}</p>
+              </Typist>
+            </article>
+            <article className='associate 3'>
+              <img className='associate_img' src={topAssociates()[2].image} alt={`mugshot of...`}></img>
+              <Typist cursor={{ hideWhenDone: true }}>
+                <p>Name: {topAssociates()[2].name}</p>
+                <p>Species: {topAssociates()[2].species}</p>
+                <p>Status: {topAssociates()[2].status}</p>
+              </Typist>
+            </article>
+          </div>
         </section>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export const mapStateToProps = ({allCharacters, currentCharacter, userName}) => ({
+export const mapStateToProps = ({
+  allCharacters,
+  currentCharacter,
+  userName
+}) => ({
   allCharacters,
   currentCharacter,
   userName
 });
 
-export default connect(mapStateToProps, null)(SingleFocus)
+export default connect(
+  mapStateToProps,
+  null
+)(SingleFocus);
