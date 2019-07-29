0dd633cbeb2b12837666bc798f2fe3f72b2f92a7
baldm0mma <jev.forsberg@gmail.com>
2019-07-29 12:43:39 -0600

    Add Card tests

--- src/containers/Card/Card.js
+++ src/containers/Card/Card.js
@@ -1,29 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Card.css';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { focusSingleCharacter } from '../../actions';
 
-export const Card = ({ character, onChoosingCriminal }) => {
-  const handleClick = () => {
-    onChoosingCriminal(character.id, character.name);
+export class Card extends Component {
+
+  handleClick = () => {
+    this.props.onChoosingCriminal(this.props.character.id, this.props.character.name);
   };
 
-  return (
-    <Link to={`/criminals/${character.name}`} className='link_to_single'>
-      <article className='allCharacter_card grow' onClick={handleClick}>
-        <h2>{character.name}</h2>
-        <img
-          className={
-            character.status === 'Dead' ? 'grid_image grey' : 'grid_image'
-          }
-          src={character.image}
-          alt={`mugshot of ${character.name}`}
-        />
-      </article>
-    </Link>
-  );
-};
+  render = () => {
+    return (
+      <Link to={`/criminals/${this.props.character.name}`} className='link_to_single'>
+        <article className='allCharacter_card grow' onClick={this.handleClick}>
+          <h2>{this.props.character.name}</h2>
+          <img
+            className={
+              this.props.character.status === 'Dead' ? 'grid_image grey' : 'grid_image'
+            }
+            src={this.props.character.image}
+            alt={`mugshot of ${this.props.character.name}`}
+          />
+        </article>
+      </Link>
+    );
+  };
+}
 
 export const mapDispatchToProps = dispatch => ({
   onChoosingCriminal: (id, name) => dispatch(focusSingleCharacter(id, name))
--- src/containers/Card/Card.test.js
+++ src/containers/Card/Card.test.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Card } from './Card';
+import { Card, mapDispatchToProps } from './Card';
 import { shallow } from 'enzyme';
+import { focusSingleCharacter } from '../../actions';
 
 describe('Card', () => {
   let wrapper;
   let character = { name: 'Mr. Poopybutthole', image: 'http://www.hi.com' };
+  let props = {
+    onChoosingCriminal: jest.fn()
+  }
   
   beforeEach(() => {
-    wrapper = shallow(<Card character={character} />);
+    wrapper = shallow(<Card character={character} {...props} />);
+  });
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
   });
 
   it('renders the name of the character wrapped in an <h2> element', () => {
@@ -15,8 +23,19 @@ describe('Card', () => {
     expect(wrapper.contains(name)).toEqual(true);
   });
 
-  it('should match the snapshot', () => {
-    expect(wrapper).toMatchSnapshot();
+  it('should have a method that handles a click, and invokes onChoosingCriminal()', () => {
+    wrapper.instance().handleClick();
+    expect(wrapper.instance().props.onChoosingCriminal).toHaveBeenCalled();
+  });
+
+  describe('mapDispatchToProps', () => {
+    it('mapDispatchToProps.onChoosingCriminal should call dispatch', () => {
+      const mockDispatch = jest.fn();
+      const actionToDispatch = focusSingleCharacter(undefined);
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.onChoosingCriminal();
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
   });
 
 });
